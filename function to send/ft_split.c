19ff9244731e1802a2b4d57dfb2fcff1dbbed42a
axel0501 <axel0501@github.com>
2021-01-25 17:35:39 +0100

    push of 25/01/2021 - h17.35

--- function to send/ft_split.c
+++ function to send/ft_split.c
@@ -6,93 +6,88 @@
 /*   By: ametta <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/21 11:52:24 by ametta            #+#    #+#             */
-/*   Updated: 2021/01/25 11:51:47 by ametta           ###   ########.fr       */
+/*   Updated: 2021/01/25 17:23:26 by ametta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-static int	ft_wc(char const *s, char const c)
+int		ft_count_words(const char *s, char c)
 {
-	size_t	i;
-	size_t	wc;
+	int i;
+	int words;
 
-	wc = 0;
 	i = 0;
-	while (s[i] != 0)
+	words = 0;
+	while (s[i])
 	{
 		if (s[i] != c)
 		{
-			wc++;
-			while (s[i] != c && s[i] != 0)
+			words++;
+			while (s[i] != c && s[i])
 				i++;
 		}
 		else
 			i++;
 	}
-	return (wc);
+	return (words);
 }
 
-static char	**ft_size_words(char **output, const char *s, const char c)
+char	**ft_size_words(const char *s, char c, char **dest)
 {
-	int	i;
-	int	j;
-	int	size;
+	int i;
+	int j;
+	int size_word;
 
 	i = 0;
 	j = 0;
-	while (s[i] != 0)
+	while (s[i])
 	{
-		size = 0;
+		size_word = 0;
 		if (s[i] != c)
 		{
-			while (s[i] != c && s[i] != 0)
+			while (s[i] != c && s[i])
 			{
-				size++;
-				i++;
+				++size_word;
+				++i;
 			}
-			output[j] = malloc(sizeof(char) * (size + 1));
+			dest[j] = malloc(sizeof(char) * (size_word + 1));
+			j++;
+			if (!dest)
+				return (NULL);
+		}
+		else
+			i++;
+	}
+	return (dest);
+}
+
+char	**ft_split(const char *s, char c)
+{
+	int		i;
+	int		j;
+	int		k;
+	char	**dest;
+
+	i = 0;
+	j = 0;
+	if (!(dest = malloc(sizeof(char*) * (ft_count_words(s, c) + 1))))
+		return (NULL);
+	if (!(ft_size_words(s, c, dest)))
+		return (NULL);
+	while (s[i] && !(k = 0))
+	{
+		if (s[i] != c)
+		{
+			while (s[i] != c && s[i])
+				dest[j][k++] = s[i++];
+			dest[j][k] = '\0';
 			j++;
 		}
 		else
 			i++;
 	}
-	return (output);
-}
-
-static void	ft_create_out(char **output, char const *s, char const delim)
-{
-	int	i;
-	int	j;
-	int	k;
-
-	i = 0;
-	j = 0;
-	while (s[i] != 0)
-	{
-	    k = 0;
-		if (s[i] != delim)
-		{
-		    while (s[i] != delim)
-		    {
-		        output[j][k++] = s[i++];
-		    }
-			output[j][k] = '\0';
-			j++;
-		}
-		i++;
-	}
-}
-
-char		**ft_split(char const *s, char c)
-{
-	char	**output;
-
-	output = malloc(sizeof(char*) * (ft_wc(s, c) + 1));
-	ft_size_words(output, s, c);
-	ft_create_out(output, s, c);
-	if (output == NULL)
-		return (NULL);
-	return (output);
+	dest[j] = NULL;
+	return (dest);
 }
--- function to send/ft_strtrim.c
+++ function to send/ft_strtrim.c
@@ -6,86 +6,22 @@
 /*   By: ametta <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/20 11:01:33 by ametta            #+#    #+#             */
-/*   Updated: 2021/01/20 11:23:25 by ametta           ###   ########.fr       */
+/*   Updated: 2021/01/25 17:21:21 by ametta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdlib.h>
 
-static int	ft_check_occ(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	i;
-	int		j;
-	size_t	occ;
+	size_t end;
 
-	occ = 0;
-	i = 0;
-	j = -1;
-	while (s1[i] != 0)
-	{
-		if (s1[i] == set[j + 1])
-			j++;
-		else if (set[j + 1] == 0)
-		{
-			occ++;
-			j = -1;
-		}
-		else if (s1[i] != set[j + 1])
-			j = -1;
-		i++;
-	}
-	return (ft_strlen(set) * occ);
-}
-
-static int	ft_control(char const *str, char const *rem, size_t c)
-{
-	int	j;
-	int	i;
-
-	i = c;
-	j = 0;
-	while (str[i] == rem[j])
-	{
-		i++;
-		j++;
-	}
-	if (rem[j] == 0)
-		return (i - 1);
-	return (-1);
-}
-
-static void	ft_create_out(char *output, char const *s1, char const *set)
-{
-	int	i;
-	int	l;
-
-	i = 0;
-	l = 0;
-	while (s1[i] != 0)
-	{
-		if (s1[i] == set[0])
-		{
-			if (ft_control(s1, set, i) == -1)
-				output[l++] = s1[i];
-			else
-				i = ft_control(s1, set, i);
-		}
-		else if (s1[i] != set[0])
-			output[l++] = s1[i];
-		i++;
-	}
-}
-
-char		*ft_strtrim(char const *s1, char const *set)
-{
-	char	*output;
-
-	if (set[0] == 0 || s1[0] == 0)
-		return (NULL);
-	output = (char*)malloc(ft_strlen(s1) - ft_check_occ(s1, set));
-	ft_create_out(output, s1, set);
-	if (output == NULL)
-		return (NULL);
-	return (output);
+	if (!s1 || !set)
+		return (0);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	end = ft_strlen(s1);
+	while (end > 0 && ft_strchr(set, s1[end]))
+		end--;
+	return (ft_substr(s1, 0, end + 1));
 }
